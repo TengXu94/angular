c201930e2aaf64f9e589f32307477c8217389d65
TengXu94 <andxu24@gmail.com>
2019-05-25 19:10:15 +0200

    268

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,22 +11,16 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { Routes, RouterModule } from '@angular/router';
 
-const appRoutes: Routes = [
-  {
-    path: 'users',
-    component: UsersComponent
-  },
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'servers',
-    component: ServersComponent
-  }
-];
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth-service';
+import { AuthGuard } from './auth-guard.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
+
+
 
 @NgModule({
   declarations: [
@@ -36,15 +30,17 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard, ServerResolver],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
